01e2f2a4af68cc88014792da1ebbaf34578cce87
Eric Tovar <tovareric@gmail.com>
2019-06-21 16:44:10 -0500

    delete function

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,8 +3,7 @@ from .import views
 
 
 urlpatterns = [
-    # path('', views.home, name='home'),
-    path('add/', views.ItemCreate.as_view(), name='item_create'),
     path('', views.ItemList.as_view(), name='item_list' ),
-    path('delete_confirm', views.ItemDelete.as_view(), name='item_delete'),
+    path('add/', views.ItemCreate.as_view(), name='item_create'),
+    path('delete_confirm/<int:pk>/', views.ItemDelete.as_view(), name='item_delete'),
 ]
